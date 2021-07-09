0b06f0e5dcc2a58e4fb50a70da88e16ac68347df
Adrian Salvador <adriplay43@gmail.com>
2021-07-09 14:32:14 +0200

    Pandas H+cb: Added NN scores from analysis

--- PandasWriter/CreatePandas_Hpcb.py
+++ PandasWriter/CreatePandas_Hpcb.py
@@ -11,7 +11,7 @@ for i in range(0,6):
     Hpcb.feature_names+=["jet"+str(i)+"_pt_bord","jet"+str(i)+"_eta_bord","jet"+str(i)+"_phi_bord","jet"+str(i)+"_m_bord","jet"+str(i)+"_y_bord","jet"+str(i)+"_btagw_discrete_bord"]
 Hpcb.feature_names+=["lep1_pt","lep1_phi","lep1_eta","met","met_phi"]
 Hpcb.feature_names+=["mbb_leading_bjets","mbb_maxdr","mbb_mindr","m_jj_leading_jets","jet0_pt","jets_n","bjets_n"]
-Hpcb.feature_names+=["nomWeight_weight_btag","nomWeight_weight_jvt","nomWeight_weight_leptonSF","nomWeight_weight_mc","nomWeight_weight_norm","nomWeight_weight_pu","event_number"]
+Hpcb.feature_names+=["nomWeight_weight_btag","nomWeight_weight_jvt","nomWeight_weight_leptonSF","nomWeight_weight_mc","nomWeight_weight_norm","nomWeight_weight_pu","event_number","scores*"]
 
 Hpcb.getGeneralSettings(configfile)
 samples = [] 
@@ -20,4 +20,4 @@ for i in ["60","70","80","90","100","110","120","130","140","150"]:
 samples+=["ttb","ttc","ttbarlight","Wtocb","Singletop","topEW","ttH","Wjets","Zjets","Dibosons","tH"]
 Hpcb.readData(samples=samples)
 Hpcb.df_mc.reset_index(inplace=True)
-Hpcb.df_mc.to_feather("pandas_Hpcb_v2.feather")
+Hpcb.df_mc.to_feather("pandas_Hpcb_v3.feather")
